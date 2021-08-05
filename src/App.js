ee3a2c45e07753c4c71ff8efdd88582ba4a4d00c
B. Kerem KURT <Kerem.Kurt@adesso.com.tr>
2021-08-05 15:05:53 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+const Main = lazy(() => import('./pages/main/index.js'));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Switch>
+        <Route exact path="/" component={Main}/>
+      </Switch>
+    </Suspense>
+  </Router>
   );
 }
 
---
+++ src/pages/main/index.js
@@ -0,0 +1,5 @@
+ const Main = () => { 
+    const x = 'Kerem';
+    return (<div> {x} </div>)
+}
+export default Main;
